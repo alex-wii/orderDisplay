96f02e925afe5f4d4c2b8a1c213e4b4572eb9a87
alex-wii <ggg.combo@gmail.com>
2021-07-19 17:06:47 +0800

    Initial import

---
+++ main3.py
@@ -0,0 +1,9 @@
+﻿import tkinter as tk
+
+
+window = tk.Tk()
+window.title('已經完成')
+window.geometry('500x100+1000+0')
+lbl_1 = tk.Label(window, text='Hello World', bg='yellow', fg='#263238', font=('Arial', 12))
+lbl_1.grid(column=0, row=0)
+window.mainloop()
---
+++ onlyok.py
@@ -0,0 +1,14 @@
+﻿import sys
+import tkinter as tk
+sys.argv[1]
+window = tk.Tk()
+window.title('已收單')
+doneCup=str(sys.argv[1])
+print(doneCup)
+window.geometry('1680x1000+0+0')
+lbl_1 = tk.Label(window, text=f'飲料 {doneCup} 正在出杯', bg='yellow', fg='#263238', font=('Arial', 170))
+lbl_1.grid(column=0, row=0)
+window.attributes('-topmost', 1)
+# window.attributes('-topmost', 0)
+window.after(20000, lambda: window.destroy()) # Destroy the widget after 30 seconds
+window.mainloop()
